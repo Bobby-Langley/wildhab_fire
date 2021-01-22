17ea7452c309805cf0d8e503987475385d3930af
Bobby Langley <bobbylangley227@gmail.com>
2021-01-22 10:24:49 -0500

    key lock2

--- categories.js
+++ categories.js
@@ -1,13 +1,3 @@
 
 
 
-firestore.collection('people').get()
-    .then(collection => {
-        collection.forEach(doc => {
-            console.log(doc.id,  '->' , doc.data())
-        })
-    })
-
-    .catch(err => console.log({err})) 
-
-    const firestore = admin.firestore()

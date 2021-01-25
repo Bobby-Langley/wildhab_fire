73382d56d9c7835619819dde6437d84971a72468
Bobby Langley <bobbylangley227@gmail.com>
2021-01-25 16:45:46 -0500

    api practice

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const {getEvents, postEvent} = require('./src/events')
+
 const app = express()
+app.use(bodyParser.json())
 const port = 3000
 
-app.get('/events', (req, res) => {
-    res.send('List of Events').status(200)
-})
+
+app.get('/events', getEvents)
+app.post('/events', postEvent)
 
 app.get('/', (req,res) => {
     res.status(200).send("hey hey hey")
 })
-
-
-
-
 
 app.listen(port, () => {
     console.log('listening on http://localhost:' + port)
---
+++ src/events/index.js
@@ -0,0 +1,79 @@
+
+const admin = require("firebase-admin");
+const serviceAccount = require("../../wildhabitatexercise-firebase-adminsdk-z62ei-e63c217782.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });  
+  const firestore = admin.firestore()
+
+const eventRef = firestore.collection('events')
+
+exports.postEvent = (req, res) => {
+    if(!firestore) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        })
+    }
+    let newEvent = req.body
+    let now = admin.firestore.FieldValue.serverTimestamp()
+    newEvent.updated = now 
+    newEvent.created = now
+
+    eventRef.add(newEvent)
+        .then(docRef => {
+            eventRef.doc(docRef.id).get()
+                .then(snapshot => {
+                    let event = snapshot.data()
+                    event.id = snapshot.id
+                    res.status(200).json({
+                        status: 'successfully successful success',
+                        data: event,
+                        message: 'Events loaded',
+                        statusCode: '200'
+                    })
+                    return 
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status().send({
+              status: 'errrrrr',
+              data: err,
+              message: 'shits broke',
+              statusCode: '500'
+          
+              }) 
+        })
+}
+
+exports.getEvents = (req, res) => {
+    if(!firestore) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        })
+    }
+     eventsRef.get()
+          .then(collection => {
+              const eventResults = collection.docs.map(doc => {
+                  let event = doc.data()
+                  event.id = doc.id
+                  return event
+              })
+              res.status(200).json({
+                  status: 'successfully successful success',
+                  data: eventResults,
+                  message: 'Events loaded',
+                  statusCode: '200'
+                })
+          })
+          .catch(err => {
+              console.log(err)
+              res.status(500).send({
+                status: 'errrrrr',
+                data: err,
+                message: 'shits broke',
+                statusCode: '500'
+           }) 
+          })
+     }
